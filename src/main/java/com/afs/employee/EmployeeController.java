cd24c6a9e211ddb852ef14a13c264c47b9a318f4
Antony Choi <antony.choi@oocl.com>
2022-12-06 16:31:20 +0800

    init

---
+++ src/main/java/com/afs/employee/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.afs.employee;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping
+    public List<Employee> getAll(){
+        return employeeRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public List<Employee> getByID(@PathVariable int id){
+        return null;
+    }
+    @GetMapping(params = {""})
+    public List<Employee> getByGender(@RequestParam String gender){
+        return null;
+    }
+}
---
+++ src/main/java/com/afs/employee/EmployeeRepository.java
@@ -0,0 +1,19 @@
+package com.afs.employee;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+    List<Employee> employees;
+
+    public EmployeeRepository(){
+        this.employees = new ArrayList<>();
+        employees.add(new Employee(1, "Lily", 20, "Female", 8000));
+    }
+    public List<Employee> findAll() {
+        return this.employees;
+    }
+}
